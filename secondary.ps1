047382fca4d282afe200881daa6c1e2f3650adef
Daniel Wale <daniel.wale@gmail.com>
2021-09-23 14:49:48 +0800

    Delete secondary.ps1

--- secondary.ps1
+++
@@ -1,7 +0,0 @@
-﻿Param(
-	[Parameter(Mandatory = $true, Position = 1)]
-    [ValidateNotNullOrEmpty()]
-    [string]
-    $ServerName = "DefaultNameIfNotProvided"
-)
-Write-Host "Secondary script ran with param ServerName param value '$ServerName'"
